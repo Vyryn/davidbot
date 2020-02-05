83fb3cd49a809e9e30fc6bc45a68d70144f6aac7
Vyryn <43193760+Vyryn@users.noreply.github.com>
2020-02-04 22:20:17 -0500

    Removed private keys.

--- privatevars.py
+++ privatevars.py
@@ -1,6 +1,6 @@
 # The private bot token
-TOKEN = 'NjcwNDQzODAxNDc0NzYwNzM0.Xiu2rQ.NRmTaraAAw-y-RXicqqC452F4nQ'
+TOKEN = ''
 # The database username
-DBUSER = 'david8'
+DBUSER = ''
 # The database password
-DBPASS = 'JOrecOC1T844V1ze3Ewo7ac46o7186'
+DBPASS = ''
