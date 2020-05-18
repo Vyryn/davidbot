c3ee5f8fd56f2baf559af1c2aa55866cfe90fde0
Vyryn <hey9055@gmail.com>
2020-05-18 16:11:07 -0400

    Added flavor to influence commands.

--- cogs/influence.py
+++ cogs/influence.py
@@ -59,7 +59,7 @@ class Influence(commands.Cog):
 
         print(f'There are {num_users} registered users in the influence system.')
         cumulative = 0
-        presentation = ''
+        presentation = 'Climb the Corporate Ladder by sending all your tributes every week! How do you stack up?\n'
         for name, number in ranks_dist.items():
             cumulative += number
             presentation += f'{name}s are in the top {int(cumulative / num_users * 1000) / 10}% of users.\n'
@@ -98,7 +98,7 @@ class Influence(commands.Cog):
             if i > amount_to_return:
                 break
             presentation += f'{i}) {user[0]}\n'
-
+        presentation = 'Climb to the top of the Corporate Ladder by sending all your tributes every week!\n'
         print(presentation)
         await ctx.send(presentation)
 
@@ -112,16 +112,19 @@ class Influence(commands.Cog):
             people[person['name']] = (person['name'], person['totalInfluence'], person['lifetimeInfluence'])
         # print(corporateer_dict)
 
-        name = ctx.author.nickname
+        name = ctx.author.nick
         if name is None:
             name = ctx.author.name
-        p_o_e = person.get(name, -1)
-        if p_o_e == -1:
+        p_o_e = people.get(name, '-1')
+        if p_o_e == '-1':
             await ctx.send("Sorry, I couldn't find you in the influence system. Make sure you're registered with "
                            "`~influence login` and that your username is exactly the same as your discord nickname.")
+            return
 
-        await ctx.send(f'{name}, your liftime influence is {people[p_o_e][2]} and your current influence balance '
-                       f'is {people[p_o_e][1]}')
+        await ctx.send(f'{name}, your liftime influence is {p_o_e[2]} and your current influence balance '
+                       f'is {p_o_e[1]}. Keep sending all your tributes to others every week to rise in the ranks. '
+                       f'Send to people you regularly play with or find helpful to help them rise in the ranks as '
+                       f'well. Remember, if you don\'t send all your tributes every week, most of it will be lost.')
 
 
 def setup(bot):
